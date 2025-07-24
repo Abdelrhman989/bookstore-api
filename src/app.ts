d5e64294118bb563b8e2213bb356cd1cf9c58faf
Mansour <bm6276492@gmail.com>
2025-07-24 13:09:44 +0300

    feat(middleware): implement catchAsync, errorHandler, notFound, and validateRequest middlewares for improved error handling and request validation

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
-
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middlewares
 app.use(express.json());
 
+// Test route
 app.get('/', (_req, res) => {
-  res.send('Welcome to the Bookstore API');
+  res.send('📚 Welcome to the Bookstore API!');
 });
 
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+// Connect to DB and Start Server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
 });
---
+++ src/middlewares/validateRequest.ts
@@ -0,0 +1,18 @@
+// src/middlewares/validateRequest.ts
+import { Request, Response, NextFunction } from 'express'
+import { ZodSchema } from 'zod'
+
+export const validateRequest = (schema: ZodSchema<any>) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body)
+      next()
+    } catch (error: any) {
+      return res.status(400).json({
+        success: false,
+        message: 'Validation error',
+        errors: error.errors || error,
+      })
+    }
+  }
+}
